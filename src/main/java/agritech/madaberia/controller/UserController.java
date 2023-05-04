5341d3ba94f9f9322cf301d563f64793ae7b2907
name <alazar.anbesi@gmail.com>
2023-05-04 18:06:54 -0500

    Update UserController.java

--- src/main/java/agritech/madaberia/controller/UserController.java
+++ src/main/java/agritech/madaberia/controller/UserController.java
@@ -10,12 +10,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import agritech.madaberia.domain.User;
 import agritech.madaberia.service.UserService;
 
-@RestController("/user/")
+@RestController
+@RequestMapping("/user/")
 public class UserController {
     @Autowired
     UserService userService;
