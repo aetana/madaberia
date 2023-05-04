236878b9845b35f7c66323ae6f4a0d37ca4055df
zadigg <abelkibebe5@gmail.com>
2023-05-04 14:12:51 -0500

    ✨ feat(model): add new model classes for Region, Zone, Woreda, and Kebele to support hierarchical location data storage and retrieval.

---
+++ src/main/java/agritech/madaberia/model/Kebele.java
@@ -0,0 +1,22 @@
+package agritech.madaberia.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Kebele {
+    @Id
+    private Long id;
+    private String name;
+    private String code;
+
+    @ManyToOne(optional = false)
+    private Woreda woreda;
+}
---
+++ src/main/java/agritech/madaberia/model/Region.java
@@ -0,0 +1,24 @@
+package agritech.madaberia.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Region {
+    @Id
+    private Long id;
+    private String name;
+    private String code;
+
+    @OneToMany(mappedBy = "region")
+    private List<Zone> zones;
+}
