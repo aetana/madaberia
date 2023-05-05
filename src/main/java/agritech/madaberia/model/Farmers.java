9b4e1893e95110bbb8e3ce24db24353b72ca583f
zadigg <abelkibebe5@gmail.com>
2023-05-04 20:22:51 -0500

    ✨ feat(MadaberiaApplication.java): add main method to run the Spring Boot application ✨ feat(Farmers.java): add Farmers entity to model farmers data ✨ feat(Kebele.java): add OneToMany relationship with Farmers entity to model farmers in a kebele

--- src/main/java/agritech/madaberia/model/Farmers.java
+++ src/main/java/agritech/madaberia/model/Farmers.java
@@ -2,28 +2,18 @@ package agritech.madaberia.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-public class Kebele {
+public class Farmers {
     @Id
     private Long id;
     private String name;
-    private String code;
-
-    @ManyToOne(optional = false)
-    private Woreda woreda;
-
-    @OneToMany
-    private List<Farmers> farmers;
-
+    private String fingerPrint;
+    private String phoneNumber;
 }
